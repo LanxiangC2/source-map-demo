782f29a2c2e9fbab41da64e58d6d8d1eb8b9e032
liuchuan <liuchuan.huang@flashwire.com>
2025-01-24 02:53:14 +0800

    feat: trigger deploy

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,6 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  // 修改
   base: './',
   plugins: [vue(), vueJsx(), vueDevTools()],
   resolve: {
