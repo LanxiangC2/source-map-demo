268b20c7123119e3541565a9208a0af1d6286233
liuchuan <liuchuan.huang@flashwire.com>
2025-01-24 02:48:44 +0800

    feat: modify base in vite.config.jg annotation

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  // 修改
   base: './',
   plugins: [vue(), vueJsx(), vueDevTools()],
   resolve: {
