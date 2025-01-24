899117cb5ae431b9bcb569d26020ae461ae8b8df
liuchuan <liuchuan.huang@flashwire.com>
2025-01-24 09:35:48 +0800

    feat: test source-map

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -2,7 +2,7 @@ import axios from 'axios'
 import SourceMap from 'source-map-js'
 
 const getSourceMap = async (url: string) => {
-  const response = await axios.post(url)
+  const response = await axios.get(url)
   return response
 }
 const findCodeBySourcemap = async (stackFrame: any) => {
@@ -11,7 +11,6 @@ const findCodeBySourcemap = async (stackFrame: any) => {
   const fileContent = sourcemap.data
 
   // 解析 sourcemap 文件
-
   const consumer = new SourceMap.SourceMapConsumer(fileContent)
   // 通过报错的位置查找对应的源文件的名称和行数
   const originalPosition = consumer.originalPositionFor({
