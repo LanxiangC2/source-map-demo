d57003c7cb3926b4cad29c860034207c5f2d7d25
LanxiangC2 <LanxiangC2@users.noreply.github.com>
2025-01-25 17:29:39 +0000

    deploy: f80a3c89e84d4742797725acf0a2dcfdc3f6a35a

---
+++ assets/eventStore-DFZ9vzYp.js
@@ -0,0 +1,1 @@
+import{g as t}from"./index-ChLkib44.js";const n=t("eventStore",{state:()=>({events:[]}),getters:{},actions:{setEvents(e){this.events=e},getEvents(){return this.events}}});export{n as u};
